5df16e5f8ee75b312bf7478a9e74b183d3b4fac5
GabrielHenri12 <gabrielhenri2002@gmail.com>
2022-12-06 12:27:59 -0300

    Edit: edit button of signOut in style

--- Front-End/src/components/header/Header.tsx
+++ Front-End/src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import Logo from '../../assets/Icone.png'
 import home from "../../assets/home.png"
 import login from "../../assets/login.png"
 import cart from "../../assets/cart.png"
+import logout from "../../assets/logout.png"
 import { Link, useNavigate } from 'react-router-dom'
 import { isAuthenticated, signOut } from '../../services/auth'
 import './Header.css'
@@ -37,7 +38,7 @@ export default () => {
                             <>
                                 <li><Link to="/"><img src={home} alt="Pagina principal" />Home</Link></li>
                                 <li><Link to="/carrinho"><img src={cart} alt="Pagina principal" />Carrinho</Link></li>
-                                <li><button onClick={e => loggout()}>Sair</button></li>
+                                <li><img src={logout} alt="deslogar do site"/><button onClick={e => loggout()}>Sair</button></li>
                             </>
                         }
                     </ul>
