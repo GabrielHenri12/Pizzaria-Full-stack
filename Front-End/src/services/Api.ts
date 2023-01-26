8ce1e65af925d2d7087ff37995d2a8bacc79ac12
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-01-26 18:53:50 -0300

    Add new link back-end

--- Front-End/src/services/Api.ts
+++ Front-End/src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { getToken } from "./auth"
 
-let url = "http://localhost:1210";
+let url = "https://pizzaria-full-stack-production.up.railway.app";
 const token = getToken();
 const api = axios.create({
     baseURL: url,
