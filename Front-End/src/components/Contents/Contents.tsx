e17e7486a8f92d05fc91d0d7ac7e21cc3cfc97fb
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-02-01 14:22:17 -0300

    configs for deploy

--- Front-End/src/components/Contents/Contents.tsx
+++ Front-End/src/components/Contents/Contents.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Cart from '../../views/Cart/Cart'
 import Home from '../../views/Home/Home'
@@ -11,12 +10,11 @@ export default () => {
   return (
     <main className='Contents'>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/cadastro' element={<Register/>}/>
-        <Route path='/carrinho' element={<Cart/>}/>
-        <Route path='*' element={<NotFound/>}/>
-          
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/cadastro" element={<Register/>}/>
+        <Route path="/carrinho" element={<Cart/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </main>
   )
