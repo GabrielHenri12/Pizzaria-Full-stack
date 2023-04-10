0d3bbc023de66a45fb86249ad0affffb89879b90
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-04-10 19:30:16 -0300

    refector:  Desacoplando codigo

--- Back-End/src/routes/index.ts
+++ Back-End/src/routes/index.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
+import { privateRouts } from "../middlewares/Auth";
 import * as pizzaController from "../controllers/pizzaController";
 import * as cartController from "../controllers/cartController";
 import * as usersController from "../controllers/usersController"
-import { privateRouts } from "../middlewares/Auth";
-import * as Validations from "../middlewares/Validators";
+import * as ValidatorsUser from "../middlewares/ValidatorsUser";
+import * as ValidatorsCart from "../middlewares/ValidatorsCart"
 
 const router = Router();
 
 router.get('/opcoes', pizzaController.home);
 router.get('/pizza/:id/', pizzaController.opcao);
-router.get('/carrinho/', privateRouts, cartController.homeCart);
-router.post('/carrinho/adicionar/', privateRouts, cartController.addCart);
-router.delete('/carrinho/:id/deletar', privateRouts, cartController.delet);
-router.post('/cadastrar/',Validations.RegisterValidator, usersController.register);
-router.post('/entrar/',Validations.LoginValidator  , usersController.login)
+router.get('/carrinho/', privateRouts, cartController.FindAllCarts);
+router.post('/carrinho/adicionar/', ValidatorsCart.CartAddValidator, privateRouts, cartController.addCart);
+router.delete('/carrinho/excluir/:id', privateRouts, cartController.delet);
+router.post('/cadastrar/', ValidatorsUser.RegisterValidator, usersController.register);
+router.post('/entrar/', ValidatorsUser.LoginValidator, usersController.login)
 
 export default router
