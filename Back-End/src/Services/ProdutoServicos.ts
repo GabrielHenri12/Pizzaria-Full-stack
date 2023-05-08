870b714785d1b34d966213ab4bddc3ce18f54a6d
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-05-08 20:48:05 -0300

    refactor: corrigindo a estrutura das class

--- Back-End/src/Services/ProdutoServicos.ts
+++ Back-End/src/Services/ProdutoServicos.ts
@@ -1,45 +1,45 @@
 import { ProdutosRepositorio } from "../Repository/ProdutosRepositorio";
-import { ProdutoConvertido } from "../utilities/ProdutosUtilities";
-import { IProdutosFuncoes } from "../Repository/IProdutosFuncoes";
+import { Conversao } from "../utilities/Conversao";
+import { IRepositorio } from "../Repository/IRepositorio";
 import { ProdutoType } from "../Types/ProdutoTypes";
 
-export class ProdutoServicos implements IProdutosFuncoes<ProdutoType>{
+export class ProdutoServicos implements IRepositorio<ProdutoType>{
     private readonly _produtosRepositorio;
-    
-    constructor(repositorio: ProdutosRepositorio){
+
+    constructor(repositorio: ProdutosRepositorio) {
         this._produtosRepositorio = repositorio
     };
-    
+
     public async Consulte(): Promise<ProdutoType[]> {
         const produtos = await this._produtosRepositorio.Consulte();
-        const produtosDTO: ProdutoType[]  = produtos?.map(produto => (ProdutoConvertido(produto)));
+        const produtosDTO: ProdutoType[] = produtos?.map(produto => (Conversao.ConverterProdutoParaType(produto)));
 
         return produtosDTO;
     }
-    
+
     public async ConsultePorID(ID: number): Promise<ProdutoType> {
         const produto = await this._produtosRepositorio.ConsultePorID(ID);
-        if(produto == null) throw new Error("Produto não encontrado");
-        const produtoDTO: ProdutoType  = ProdutoConvertido(produto);
+        if (produto == null) throw new Error("Produto não encontrado");
+        const produtoDTO: ProdutoType = Conversao.ConverterProdutoParaType(produto);
 
         return produtoDTO;
     }
 
-    ConsulteParcial(): Promise<ProdutoType[]> {
+    ConsulteParcial(valor: string): Promise<ProdutoType | null> {
         throw new Error("Method not implemented.");
     }
 
 
-    Adicionar(dados: ProdutoType): Promise<ProdutoType> {
+    Adicionar(dados: ProdutoType): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    Editar(dados: ProdutoType): Promise<ProdutoType> {
+    Editar(dados: ProdutoType): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
     public async Deletar(ID: number): Promise<void> {
         await this._produtosRepositorio.Deletar(ID);
     }
-    
+
 }
