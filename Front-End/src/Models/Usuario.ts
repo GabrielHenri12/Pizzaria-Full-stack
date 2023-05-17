aa79c5c1a64583a1eff7c325379c0653440ec3cf
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-05-17 19:47:21 -0300

    feat: Criando arquivos model

---
+++ Front-End/src/Models/Usuario.ts
@@ -0,0 +1,12 @@
+export type UsuarioType= {
+    ID: number;
+    NOME: string;
+    SOBRENOME: string;
+    CPF: string;
+    CREDENCIAL: string;
+    TELEFONE: string;
+    IDADE: number;
+    EMAIL: string;
+    SENHA: string;
+    TOKEN: string;
+}
