432623464610daeaa8fbfea1589262467581de4e
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-05-23 22:41:15 -0300

    Nomeclatura: Corrigindo nomeclaturas

--- Back-End/src/routes/index.ts
+++ Back-End/src/routes/index.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
 import { privateRouts } from "../middlewares/Auth";
 import ProdutoController from "../controllers/ProdutoController";
-import * as cartController from "../controllers/cartController";
+import * as PedidoController from "../controllers/PedidoController";
 import UsuarioController from "../controllers/usersController"
 import * as ValidatorsUser from "../middlewares/ValidadorUsuario";
-import * as ValidatorsCart from "../middlewares/ValidatorsCart"
+import * as ValidatorsCart from "../middlewares/ValidadorPedido"
 
 const router = Router();
 
 router.get('/produtos', ProdutoController.Consulte);
 router.get('/produto/:id/', ProdutoController.ConsultePorID);
-router.get('/carrinho/', privateRouts, cartController.FindAllCarts);
-router.post('/carrinho/adicionar/', ValidatorsCart.CartAddValidator, privateRouts, cartController.addCart);
-router.delete('/carrinho/excluir/:id', privateRouts, cartController.delet);
+router.get('/carrinho/', privateRouts, PedidoController.ConsultePorUsuario);
+router.post('/carrinho/adicionar/', privateRouts, ValidatorsCart.CartAddValidator, PedidoController.Adicionar);
+router.delete('/carrinho/excluir/:id', privateRouts, PedidoController.delet);
 router.post('/cadastrar/', ValidatorsUser.ValidadorRegistro, UsuarioController.register);
 router.post('/entrar/', ValidatorsUser.ValidadorLogin, UsuarioController.Logar);
 
