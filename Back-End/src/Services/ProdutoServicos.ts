94172d12760760fe0be6677acfddaa729ef1820b
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-05-23 22:50:04 -0300

    refactor: adicionando callback aos metodos

--- Back-End/src/Services/ProdutoServicos.ts
+++ Back-End/src/Services/ProdutoServicos.ts
@@ -1,41 +1,30 @@
-import { ProdutosRepositorio } from "../Repository/ProdutosRepositorio";
+import { Callback, Resultado, Erro } from "../utilities/Callback";
+import { ErrorCustom } from "../Error/ErrorType";
 import { Conversao } from "../utilities/Conversao";
-import { IRepositorio } from "../Repository/IRepositorio";
+import { IProdutoRepositorio } from "../Repository/IProdutoRepositorio";
 import { ProdutoType } from "../Types/ProdutoTypes";
 
-export class ProdutoServicos implements IRepositorio<ProdutoType>{
+export class ProdutoServicos{
     private readonly _produtosRepositorio;
 
-    constructor(repositorio: ProdutosRepositorio) {
+    constructor(repositorio: IProdutoRepositorio) {
         this._produtosRepositorio = repositorio
     };
 
-    public async Consulte(): Promise<ProdutoType[]> {
+    public async Consulte(): Promise<Callback<ErrorCustom, ProdutoType[]>> {
         const produtos = await this._produtosRepositorio.Consulte();
-        const produtosDTO: ProdutoType[] = produtos?.map(produto => (Conversao.ConverterProdutoParaType(produto)));
-
-        return produtosDTO;
+        if(!produtos) return Erro(new ErrorCustom("Lista vazia", false, 404))
+        const produtosDTO: ProdutoType[] = produtos.map(produto => (Conversao.ConverterProdutoParaType(produto)));
+        
+        return Resultado(produtosDTO);
     }
 
-    public async ConsultePorID(ID: number): Promise<ProdutoType> {
+    public async ConsultePorID(ID: number): Promise<Callback<ErrorCustom, ProdutoType>> {
         const produto = await this._produtosRepositorio.ConsultePorID(ID);
-        if (produto == null) throw new Error("Produto não encontrado");
+        if(!produto) return Erro(new ErrorCustom("Produto não encontrado", false, 404))
         const produtoDTO: ProdutoType = Conversao.ConverterProdutoParaType(produto);
 
-        return produtoDTO;
-    }
-
-    ConsulteParcial(valor: string): Promise<ProdutoType | null> {
-        throw new Error("Method not implemented.");
-    }
-
-
-    Adicionar(dados: ProdutoType): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-
-    Editar(dados: ProdutoType): Promise<void> {
-        throw new Error("Method not implemented.");
+        return Resultado(produtoDTO);
     }
 
     public async Deletar(ID: number): Promise<void> {
