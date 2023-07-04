d2efe4ff09429dfdf76c6328bba09fb7b8dec75d
Gabriel Henrique <gabrielhenri2002@gmail.com>
2023-07-03 22:05:43 -0300

    Nomeclatura: aletando o nome da chave de retorno

--- Back-End/src/Error/ErrorHandler.ts
+++ Back-End/src/Error/ErrorHandler.ts
@@ -1,12 +1,7 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import { ErrorCustom } from "./ErrorType";
 
-export const errorHandler: ErrorRequestHandler = (err: ErrorCustom, req: Request, res: Response, next: NextFunction) => {
-    err.status ?
-        res.status(err.statusCode) :
-        res.status(400);
-
+export const errorHandler: ErrorRequestHandler = (err: ErrorCustom, req: Request, res: Response, next: NextFunction) =>
     err.message ?
-        res.json({ status: false, data: err.message }) :
-        res.json({ status: false, data: 'Ocorreu algum erro.' });
-}
+        res.status(err.statusCode).json({ status: false, value: err.message }) :
+        res.status(err.statusCode).json({ status: false, value: 'Ocorreu algum erro no servidor!.' });
