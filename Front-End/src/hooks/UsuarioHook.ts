eac1bc1b1a14999a47970a03eedf568e931bf9a5
Gabriel Henrique <gabrielhenri2002@gmail.com>
2023-07-06 20:38:35 -0300

    feat: Finalizando hook para usuario

--- Front-End/src/hooks/UsuarioHook.ts
+++ Front-End/src/hooks/UsuarioHook.ts
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { UsuarioType } from "../Types/Usuario";
 import { MyContext } from "../Contexts/Context";
 import api from "../services/Api";
+import { useNavigate } from "react-router-dom";
 
 const initUsuario: UsuarioType = {
     ID: 0,
@@ -17,16 +18,32 @@ const initUsuario: UsuarioType = {
 }
 
 export const UsuarioHook = () => {
-    const { dispatch } = useContext(MyContext)
-    const [usuario, setUsuario] = useState<UsuarioType>(initUsuario)
+    const { dispatch } = useContext(MyContext);
+    const [usuario, setUsuario] = useState<UsuarioType>(initUsuario);
+    const navigate = useNavigate();
 
     const handleChangeLogin = async () => {
         const response = await api.post("/entrar", { EMAIL: usuario.EMAIL, SENHA: usuario.SENHA })
+        const { status, value } = response.data;
+
         dispatch({
             type: "Login",
-            payload: {token: response.data.value}
+            payload: { token: value }
         })
+        if (status) {
+            navigate("/");
+        }
     }
+
+    const handleChangeRegister = async () => {
+        const response = await api.post("/cadastrar", usuario)
+        const { status } = response.data;
+
+        if (status) {
+            navigate("/usuario");
+        }
+    }
+
     const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { id, value } = event.target;
         setUsuario((usuarioPrev) => ({ ...usuarioPrev, [id]: value }))
@@ -35,6 +52,7 @@ export const UsuarioHook = () => {
     return {
         usuario,
         handleChangeInput,
-        handleChangeLogin
+        handleChangeLogin,
+        handleChangeRegister
     }
 }
