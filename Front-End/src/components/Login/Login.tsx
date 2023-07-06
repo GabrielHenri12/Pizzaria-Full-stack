57c8be7feedaa4e1f6b49f697da478617fc94af6
Gabriel Henrique <gabrielhenri2002@gmail.com>
2023-07-05 22:57:46 -0300

    feat: criando as funcionalidades da tela de login

--- Front-End/src/components/Login/Login.tsx
+++ Front-End/src/components/Login/Login.tsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+import { UsuarioHook } from "../../hooks/UsuarioHook"
+
 export default () => {
+    const { usuario, handleChangeInput, handleChangeLogin } = UsuarioHook();
+    
     return (
-        <>
-        </>
+        <div>
+            <form onSubmit={(e) => e.preventDefault()}>
+                <h1>Fazer Login</h1>
+                <label htmlFor="EMAIL">Email</label>
+                <input id="EMAIL" value={usuario.EMAIL} onChange={handleChangeInput} required />
+                <label htmlFor="SENHA">Senha</label>
+                <input id="SENHA" value={usuario.SENHA} onChange={handleChangeInput} required />
+                <button onClick={handleChangeLogin}>Entrar</button>
+            </form>
+            <Link to="cadastrar">Cadastrar-se</Link>
+        </div>
     )
 }
