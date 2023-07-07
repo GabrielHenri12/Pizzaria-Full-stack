665b508249b2f4ed3153848184d2c248dc2b98a3
Gabriel Henrique <gabrielhenri2002@gmail.com>
2023-07-06 21:18:31 -0300

    feat: nova função de notificações

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -4,12 +4,16 @@ import Contents from './components/Contents/Contents'
 import Header from './components/header/Header'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import ContextProvider from './Contexts/Context'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 const queryClient = new QueryClient()
 
 export default () => {
+
   return (
     <div className='App'>
+      <ToastContainer />
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <ContextProvider>
--- Front-End/src/services/Api.ts
+++ Front-End/src/services/Api.ts
@@ -1,12 +1,13 @@
-import axios from "axios"
-import { getToken } from "./auth"
+import axios from "axios";
+import { getToken } from "./auth";
+import { toast } from 'react-toastify';
 
 let url = "https://apigh.azurewebsites.net/";
 let urlLocal = "http://localhost:8080/"
 
 const token = getToken();
 const api = axios.create({
-    baseURL: url,
+    baseURL: urlLocal,
     headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
@@ -15,7 +16,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     config => {
-        
+
         if (token) {
             api.defaults.headers.authorization = `Bearer ${token}`;
         }
@@ -25,5 +26,13 @@ api.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+api.interceptors.response.use(
+    response => response,
+    error => {
+        toast.error(error.response.data.value)
+        return Promise.reject(error);
+    }
+);
+
 
 export default api;
